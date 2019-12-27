0af153ab01848ed82d3428780f48265712aae468
Juan Rodríguez <jsrd98@gmail.com>
2019-12-27 06:38:09 -0500

    refactor: Group anagrams memory usage improvement

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -9,14 +9,13 @@
 var groupAnagrams = function(strs) {
     const hashMap = new Map();
     for (let i = 0; i < strs.length; i++) {
-        // We need to match each word with an unique key
-        // that have exactly the same letters.
-        // The sorted word will match with all of them
+        // Sorting the word we get an unique key for each anagram
         let key = strs[i].split('').sort().join('');
-        let words = hashMap.get(key) || [];
-        // Push the word in the respective key-array
-        words.push(strs[i]);
-        hashMap.set(key, words);
+        if (hashMap.has(key)) {
+            hashMap.set(key, [...hashMap.get(key), strs[i]]);
+        } else {
+            hashMap.set(key, [strs[i]]);
+        }
     }
     return [...hashMap.values()];
 };
